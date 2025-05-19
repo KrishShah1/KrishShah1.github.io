f4a1974cf6cfebcc1f657ab70b645bab89a6866f
KrishShah1 <krishshah8590@gmail.com>
2025-05-18 20:28:08 -0700

    Game made

--- merge-game/src/game/main.js
+++ merge-game/src/game/main.js
@@ -1,31 +1,17 @@
-import { Boot } from './scenes/Boot';
-import { Game } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
+// src/game/main.js
 import Phaser from 'phaser';
-import { Preloader } from './scenes/Preloader';
+import GameScene from './scenes/GameScene';
 
-// Find out more information about the Game Config at:
-// https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
-const config = {
+export default function createGame(containerId) {
+  return new Phaser.Game({
     type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        Game,
-        GameOver
-    ]
-};
-
-const StartGame = (parent) => {
-
-    return new Phaser.Game({ ...config, parent });
-
+    parent: containerId,
+    width: 800,
+    height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: { gravity: { y: 300 }, debug: false }
+    },
+    scene: [ GameScene ]
+  });
 }
-
-export default StartGame;
